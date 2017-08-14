32cb4bb0ed0f9956c131a8a651a9977241c2dbfc
Chad Yates <cayates1989@gmail.com>
2017-08-14 18:30:33 -0400

    final commit with everything, including chalk colors, committed

--- app/datefacts.js
+++ app/datefacts.js
@@ -1,6 +1,6 @@
 var momentInTime = require('moment');
 var chalk = require('chalk');
-let currentMomentInTime = momentInTime([2017, 11, 14]).isDST()
+let currentMomentInTime = momentInTime([2017, 8, 14]).isDST()
 
 
 
@@ -12,49 +12,16 @@ let minutes = momentInTime().minutes();
 let hours = momentInTime().hours();
 let seconds = momentInTime().seconds();
 
-console.log("It is " + ((hours*60*60) + (minutes*60) + (seconds)) + " seconds into the day.");
+console.log("It is " + (chalk.cyan((hours*60*60) + (minutes*60) + (seconds))) + " seconds into the day.");
 
-if (currentMomentInTime = true){ // i need to check this because my currentMoment variable is always coming back as true regardless of the time of year
-    console.log("It is during Daylight Savings time.")
+if (currentMomentInTime === true){
+    console.log("It " + (chalk.green("is")) + " during Daylight Savings time.")
 }
     else {(console.log("It is not during Daylight Savings time."))         
 }
 
+let year = momentInTime().year();
+let leapYear = momentInTime([year]).isLeapYear();
 
-console.log("")
-console.log(currentMomentInTime)
-
-
-
-
-
-
-// const moment = require('node-moment');
-
-// var nextyear  = moment().add(moment.duration("P1Y"));
-// console.log("Next year will be", nextyear);
-
-
-// //load languages (if required)
-// var moment_fr = require('node-moment/lang/fr');
-// moment_fr(moment);
-
-// var nextyear  = moment().add(moment.duration("P1Y"));
-// console.log("Next year will be", nextyear);
-
-
-
-
-// const readline = require('readline');
-
-// const rl = readline.createInterface({
-//   input: process.stdin,
-//   output: process.stdout
-// });
-
-// rl.question('How often do you fart? ', (answer) => {
-//   // TODO: Log the answer in a database
-//   console.log("I have farted " + `${answer.length}` + " times today");
-
-//   rl.close();
-// });
+if (leapYear === false){
+    console.log("It " + (chalk.red("is not") + " a leap year"))}

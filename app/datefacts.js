a9b94ae742ba3a4ac7b2a85a26f2fc5385c7d948
Chad Yates <cayates1989@gmail.com>
2017-08-14 16:24:02 -0400

    first commit

---
+++ app/datefacts.js
@@ -0,0 +1,25 @@
+
+
+
+
+
+
+
+
+
+
+
+
+// const readline = require('readline');
+
+// const rl = readline.createInterface({
+//   input: process.stdin,
+//   output: process.stdout
+// });
+
+// rl.question('How often do you fart? ', (answer) => {
+//   // TODO: Log the answer in a database
+//   console.log("I have farted " + `${answer.length}` + " times today");
+
+//   rl.close();
+// });
